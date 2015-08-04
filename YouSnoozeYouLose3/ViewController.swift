fb74ab282e174195b91fe75d6fd6322ec1101edb
tyh24647 <tyhostager@yahoo.com>
2015-08-03 21:21:05 -0500

    Initial Commit

---
+++ YouSnoozeYouLose3/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  YouSnoozeYouLose3
+//
+//  Created by Tyler hostager on 8/3/15.
+//  Copyright © 2015 Tyler hostager. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
